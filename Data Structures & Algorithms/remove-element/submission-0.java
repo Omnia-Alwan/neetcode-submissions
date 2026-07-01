38c6a01ecc6aa4187ff3eeb4fe6c39c42f41ef8c
Omnia-Alwan <ommah899@gmail.com>
2026-07-02 02:42:56 +0300

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                k++;
+                nums[k-1]=nums[i];
+            }
+        }
+        return k;
+    }
+}
